1dd0d88993508a92cf658dd1a3956ce77ebc97ce
Anatolii <gumby@ukr.net>
2019-11-29 00:40:38 +0200

    sss

--- Src/main.c
+++ Src/main.c
@@ -23,7 +23,14 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+/* vim: set ai et ts=4 sw=4: */
+#include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "socket.h"
+#include "dhcp.h"
+#include "dns.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -59,12 +66,211 @@ static void MX_I2C1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
+void Ajsadjsadlkasjdakjdalkjdsjlksadlksaj(void) {
+	HAL_Delay(1000);
+	HAL_Delay(1000);
+	HAL_Delay(1000);
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+#define DHCP_SOCKET     0
+#define DNS_SOCKET      1
+#define HTTP_SOCKET     2
+
+void UART_Printf(const char* fmt, ...) {
+    char buff[256];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buff, sizeof(buff), fmt, args);
+    HAL_UART_Transmit(&huart1, (uint8_t*)buff, strlen(buff),
+                      HAL_MAX_DELAY);
+    va_end(args);
+}
+
+void W5500_Select(void) {
+    HAL_GPIO_WritePin(W5500_CS_GPIO_Port, W5500_CS_Pin, GPIO_PIN_RESET);
+}
+
+void W5500_Unselect(void) {
+    HAL_GPIO_WritePin(W5500_CS_GPIO_Port, W5500_CS_Pin, GPIO_PIN_SET);
+}
+
+void W5500_ReadBuff(uint8_t* buff, uint16_t len) {
+    HAL_SPI_Receive(&hspi1, buff, len, HAL_MAX_DELAY);
+}
+
+void W5500_WriteBuff(uint8_t* buff, uint16_t len) {
+    HAL_SPI_Transmit(&hspi1, buff, len, HAL_MAX_DELAY);
+}
+
+uint8_t W5500_ReadByte(void) {
+    uint8_t byte;
+    W5500_ReadBuff(&byte, sizeof(byte));
+    return byte;
+}
+
+void W5500_WriteByte(uint8_t byte) {
+    W5500_WriteBuff(&byte, sizeof(byte));
+}
+
+volatile bool ip_assigned = false;
+
+void Callback_IPAssigned(void) {
+    UART_Printf("Callback: IP assigned! Leased time: %d sec\r\n", getDHCPLeasetime());
+    ip_assigned = true;
+}
+
+void Callback_IPConflict(void) {
+    UART_Printf("Callback: IP conflict!\r\n");
+}
+
+// 1K should be enough, see https://forum.wiznet.io/t/topic/1612/2
+//uint8_t dhcp_buffer[2];
+// 1K seems to be enough for this buffer as well
+//uint8_t dns_buffer[2];
+
+void init() {
+//	Ajsadjsadlkasjdakjdalkjdsjlksadlksaj();
+//    UART_Printf("\r\ninit() called!\r\n");
+//
+//    UART_Printf("Registering W5500 callbacks...\r\n");
+    HAL_GPIO_WritePin(W5500_CS_GPIO_Port, W5500_CS_Pin, GPIO_PIN_SET);
+
+
+    reg_wizchip_cs_cbfunc(W5500_Select, W5500_Unselect);
+    reg_wizchip_spi_cbfunc(W5500_ReadByte, W5500_WriteByte);
+    reg_wizchip_spiburst_cbfunc(W5500_ReadBuff, W5500_WriteBuff);
+
+//    UART_Printf("Calling wizchip_init()...\r\n");
+    uint8_t rx_tx_buff_sizes[] = {2, 2, 2, 2, 2, 2, 2, 2};
+    wizchip_init(rx_tx_buff_sizes, rx_tx_buff_sizes);
+
+//    UART_Printf("Calling DHCP_init()...\r\n");
+    wiz_NetInfo net_info = {
+        .mac  = { 0xEA, 0x11, 0x22, 0x33, 0x44, 0xEA },
+        .dhcp = NETINFO_DHCP,
+		.ip = {192,168,0,5},
+		.sn = {255,255,255,0}
+    };
+    // set MAC address before using DHCP
+    setSHAR(net_info.mac);
+//    DHCP_init(DHCP_SOCKET, dhcp_buffer);
+
+//    UART_Printf("Registering DHCP callbacks...\r\n");
+    reg_dhcp_cbfunc(
+        Callback_IPAssigned,
+        Callback_IPAssigned,
+        Callback_IPConflict
+    );
+
+//    UART_Printf("Calling DHCP_run()...\r\n");
+    // actually should be called in a loop, e.g. by timer
+//    uint32_t ctr = 10000;
+//    while((!ip_assigned) && (ctr > 0)) {
+//        DHCP_run();
+//        ctr--;
+//    }
+//    if(!ip_assigned) {
+//        UART_Printf("\r\nIP was not assigned :(\r\n");
+//        return;
+//    }
+
+//    getIPfromDHCP(net_info.ip);
+//    getGWfromDHCP(net_info.gw);
+//    getSNfromDHCP(net_info.sn);
+//
+//    uint8_t dns[4];
+//    getDNSfromDHCP(dns);
+
+//    UART_Printf("IP:  %d.%d.%d.%d\r\nGW:  %d.%d.%d.%d\r\nNet: %d.%d.%d.%d\r\nDNS: %d.%d.%d.%d\r\n",
+//        net_info.ip[0], net_info.ip[1], net_info.ip[2], net_info.ip[3],
+//        net_info.gw[0], net_info.gw[1], net_info.gw[2], net_info.gw[3],
+//        net_info.sn[0], net_info.sn[1], net_info.sn[2], net_info.sn[3],
+//        dns[0], dns[1], dns[2], dns[3]
+//    );
+
+//    UART_Printf("Calling wizchip_setnetinfo()...\r\n");
+    wizchip_setnetinfo(&net_info);
+
+//    UART_Printf("Calling DNS_init()...\r\n");
+//    DNS_init(DNS_SOCKET, dns_buffer);
+
+//    uint8_t addr[4];
+//    {
+//        char domain_name[] = "eax.me";
+//        UART_Printf("Resolving domain name \"%s\"...\r\n", domain_name);
+//        int8_t res = DNS_run(dns, (uint8_t*)&domain_name, addr);
+//        if(res != 1) {
+//            UART_Printf("DNS_run() failed, res = %d", res);
+//            return;
+//        }
+//        UART_Printf("Result: %d.%d.%d.%d\r\n", addr[0], addr[1], addr[2], addr[3]);
+//    }
+
+//    UART_Printf("Creating socket...\r\n");
+    uint8_t http_socket = HTTP_SOCKET;
+    uint8_t code = socket(http_socket, Sn_MR_TCP, 10888, 0);
+    if(code != http_socket) {
+        UART_Printf("socket() failed, code = %d\r\n", code);
+        return;
+    }
+
+//    UART_Printf("Socket created, connecting...\r\n");
+//    code = connect(http_socket, addr, 80);
+//    if(code != SOCK_OK) {
+//        UART_Printf("connect() failed, code = %d\r\n", code);
+//        close(http_socket);
+//        return;
+//    }
+
+//    UART_Printf("Connected, sending HTTP request...\r\n");
+//    {
+//        char req[] = "GET / HTTP/1.0\r\nHost: eax.me\r\n\r\n";
+//        uint16_t len = sizeof(req) - 1;
+//        uint8_t* buff = (uint8_t*)&req;
+//        while(len > 0) {
+//            UART_Printf("Sending %d bytes...\r\n", len);
+//            int32_t nbytes = send(http_socket, buff, len);
+//            if(nbytes <= 0) {
+//                UART_Printf("send() failed, %d returned\r\n", nbytes);
+//                close(http_socket);
+//                return;
+//            }
+//            UART_Printf("%d bytes sent!\r\n", nbytes);
+//            len -= nbytes;
+//        }
+//    }
+
+//    UART_Printf("Request sent. Reading response...\r\n");
+//    {
+//        char buff[32];
+//        for(;;) {
+//            int32_t nbytes = recv(http_socket, (uint8_t*)&buff, sizeof(buff)-1);
+//            if(nbytes == SOCKERR_SOCKSTATUS) {
+//                UART_Printf("\r\nConnection closed.\r\n");
+//                break;
+//            }
+//
+//            if(nbytes <= 0) {
+//                UART_Printf("\r\nrecv() failed, %d returned\r\n", nbytes);
+//                break;
+//            }
+//
+//            buff[nbytes] = '\0';
+//            UART_Printf("%s", buff);
+//        }
+//    }
+
+//    UART_Printf("Closing socket.\r\n");
+//    close(http_socket);
+}
+
+void loop() {
+    HAL_Delay(1000);
+}
 /* USER CODE END 0 */
 
 /**
@@ -95,11 +301,29 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+
+
+
   MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_SPI1_Init();
-  MX_USART1_UART_Init();
+
+  HAL_GPIO_WritePin(ULED_GPIO_Port, ULED_Pin, GPIO_PIN_SET);
+  HAL_Delay(1000);
+  HAL_GPIO_WritePin(ULED_GPIO_Port, ULED_Pin, GPIO_PIN_RESET);
+  HAL_Delay(1000);
+  HAL_GPIO_WritePin(ULED_GPIO_Port, ULED_Pin, GPIO_PIN_SET);
+
+//  MX_I2C1_Init();
+//  MX_SPI1_Init();
+//  MX_USART1_UART_Init();
+
+
   /* USER CODE BEGIN 2 */
+  HAL_GPIO_TogglePin(ULED_GPIO_Port, ULED_Pin);
+  HAL_Delay(1000);
+  HAL_GPIO_TogglePin(ULED_GPIO_Port, ULED_Pin);
+  HAL_Delay(1000);
+  HAL_GPIO_TogglePin(ULED_GPIO_Port, ULED_Pin);
+  init();
 
   /* USER CODE END 2 */
 
@@ -107,6 +331,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	loop();
+    HAL_GPIO_TogglePin(ULED_GPIO_Port, ULED_Pin);
+
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
